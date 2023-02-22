d239ee8a28b6054da85f5888e0c34bec2efab226
Yor-in <65022432@up.ac.th>
2023-02-22 14:55:48 +0700

    Add test

---
+++ script.js
@@ -0,0 +1,12 @@
+function click() {
+    const Txt1 = document.getElementById("namein").value;
+    const Txt2 = document.getElementById('surnamein').value;
+    const Txt3 = document.getElementById('genderin').value;
+    const Txt4 = document.getElementById('birthdayin').value;
+    alert("ข้อมูลไม่ครบ");
+    if (Txtappear1 === ''){
+        alert("ข้อมูลไม่ครบ");
+    }else{
+        alert("ข้อมูลไม่ครบ");
+    }
+}
